ac9a61f7752939ea41d38687a033b7d7f0bc8c00
Ernesto Cianciotta <ernesto.cianciotta@gmail.com>
2023-08-27 19:00:39 +0200

    Aligned Context.cs to CLI version.

--- SolutionSecrets.Core/Context.cs
+++ SolutionSecrets.Core/Context.cs
@@ -16,7 +16,7 @@ namespace SolutionSecrets.Core
         public IConsoleInput Input => GetService<IConsoleInput>();
         public IFileSystem IO => GetService<IFileSystem>();
         public ICipher Cipher => GetService<ICipher>();
-        public IRepository Repository => GetService<IRepository>();
+        public IRepository Repository => GetRepository(SyncConfiguration.Default) ?? GetService<IRepository>();
 
 
         private static Context _current;
@@ -25,11 +25,13 @@ namespace SolutionSecrets.Core
 
 
         private IDictionary<string, object> _services;
+        private IDictionary<Type, ISet<object>> _servicesByType;
 
 
         private Context()
         {
             _services = new Dictionary<string, object>();
+            _servicesByType = new Dictionary<Type, ISet<object>>();
             ResetToDefault();
         }
 
@@ -39,7 +41,8 @@ namespace SolutionSecrets.Core
             if (service == null)
                 throw new ArgumentNullException(nameof(service));
 
-            var key = typeof(T).FullName;
+            var type = typeof(T);
+            var key = type.FullName;
             if (key == null)
                 throw new InvalidOperationException("The service cannot be added as a dependency.");
 
@@ -49,6 +52,14 @@ namespace SolutionSecrets.Core
             }
 
             _services[key] = service;
+
+            ISet<object> servicesByType;
+            if (!_servicesByType.TryGetValue(type, out servicesByType))
+            {
+                servicesByType = new HashSet<object>();
+                _servicesByType[type] = servicesByType;
+            }
+            servicesByType.Add(service);
         }
 
 
@@ -71,12 +82,34 @@ namespace SolutionSecrets.Core
         }
 
 
+        public ISet<T> GetServices<T>() where T: class
+        {
+            var type = typeof(T);
+            var services = new HashSet<T>();
+            if (_servicesByType.TryGetValue(type, out var servicesForType))
+            {
+                foreach (var service in servicesForType)
+                {
+                    services.Add((T)service);
+                }
+            }
+            return services;
+        }
+
+
+        public void Clear()
+        {
+            _services.Clear();
+            _servicesByType.Clear();
+        }
+
+
         private static IConsoleInput defaultConsoleInput = new ConsoleInput();
         private static IFileSystem defaultIO = new DefaultFileSystem();
 
         public void ResetToDefault()
         {
-            _services.Clear();
+            Clear();
             AddService(defaultConsoleInput);
             AddService(defaultIO);
         }
